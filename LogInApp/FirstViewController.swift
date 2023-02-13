f46d9b49423edaf4709cbe0069f979433c8c5b85
DZH4F4R0V <jeff-122@ya.ru>
2023-02-13 23:44:07 +0300

    ViewController setup

--- LogInApp/FirstViewController.swift
+++ LogInApp/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  LogInApp
 //
 //  Created by J Eff on 13.02.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
+
 
---
+++ LogInApp/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by J Eff on 13.02.2023.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
