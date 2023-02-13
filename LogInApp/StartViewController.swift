b091b88c33382941869e7bd00feeea1a6b90119c
DZH4F4R0V <jeff-122@ya.ru>
2023-02-14 00:58:08 +0300

    Stacked

--- LogInApp/StartViewController.swift
+++ LogInApp/StartViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 final class StartViewController: UIViewController {
-
+    
+    @IBOutlet var StartVC: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        StartVC.setGradientBackground(colorTop: UIColor.blue, colorBottom: UIColor.green)
+    }
+    
+    private func setupBackground(colorTop: UIColor, colorBottom: UIColor) {
+        let gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [colorBottom.cgColor, colorTop.cgColor]
+            gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+            gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+            gradientLayer.locations = [0, 1]
+        gradientLayer.frame = UIGraphicsGetPDFContextBounds()
 
-
+        StartVC.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
 }
-
